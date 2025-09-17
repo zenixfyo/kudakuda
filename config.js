4c2a135c06ea29de97fee95ae9fc190e950b3551
zenixfyo <apendape06@gmail.com>
2025-09-17 18:03:19 +0700

    upload file: config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// File: config.js
+
+const config = {
+  // GANTI SEMUA VALUE DI BAWAH INI SESUAI PENGATURAN PANELMU
+  domain: "https://zebubhost.cloud",
+  
+  // 🚨 BAHAYA: JANGAN PERNAH UPLOAD FILE INI KE GITHUB PUBLIK JIKA APIKEY SUDAH DIISI!
+  apikey: "PTLC_xxxxxxxxxxxxxxxxxxxxxxxx", // <-- GANTI DENGAN API KEY ADMIN PANELMU
+  
+  // Pengaturan untuk pembuatan server
+  egg: "15",
+  nestid: "5",
+  loc: "1"
+};
+
+export default config;
